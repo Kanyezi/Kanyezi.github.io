f56b48f27fd09ed962699f6aaad64a24ba86cf10
GaiShen <1214241948@qq.com>
2025-11-20 18:50:15 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   pages/dist/all_data.json
    	new file:   pages/dist/atcoder/2025-11-20_atcoder.json
    	new file:   pages/dist/codeforces/2025-11-20_codeforces.json
    	modified:   pages/dist/file_list.json
    	modified:   pages/dist/index.js
    	new file:   pages/dist/matiji/2025-11-20_matiji.json
    	modified:   pages/get_pachong/__pycache__/atcoder_get.cpython-312.pyc
    	modified:   pages/get_pachong/__pycache__/codeforces_get.cpython-312.pyc
    	modified:   pages/get_pachong/__pycache__/matiji_get.cpython-312.pyc
    	modified:   pages/get_pachong/matiji_get.py
    	modified:   pages/public/all_data.json
    	new file:   pages/public/atcoder/2025-11-20_atcoder.json
    	new file:   pages/public/codeforces/2025-11-20_codeforces.json
    	modified:   pages/public/file_list.json
    	new file:   pages/public/matiji/2025-11-20_matiji.json

--- pages/get_pachong/matiji_get.py
+++ pages/get_pachong/matiji_get.py
@@ -34,10 +34,10 @@ list=[
 ]
 
 def get_user_ac_count(id):
-    url = "https://www.matiji.net/exam-back/api/queryUserDetailById.do"
+    url = "https://www.matiji.net/exam-back/pc/queryUserDetailById.do"
     header={
         "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0",
-        "cookie":"JSESSIONID=59DEFD9DC93C0EF9B4E99CE9C7D436A0; Hm_lvt_91dd75297031f8bd0eaeb64ef3d6e20e=1760014858,1760015071,1760856215; HMACCOUNT=60B37D0CFB75D3DB; Hm_lpvt_91dd75297031f8bd0eaeb64ef3d6e20e=1760857598; SERVERID=b94def476a02d1e749557bf71c4ff9e4|1760857594|1760856200"
+        "cookie":"JSESSIONID=18E11AFBE1DDF7C6933A183F7A33F819; Hm_lvt_91dd75297031f8bd0eaeb64ef3d6e20e=1763635541; Hm_lpvt_91dd75297031f8bd0eaeb64ef3d6e20e=1763635541; HMACCOUNT=60B37D0CFB75D3DB; SERVERID=b94def476a02d1e749557bf71c4ff9e4|1763635544|1763635501"
     }
     data={"userId" : id}
     requ = requests.post(url=url,headers=header,data=data);
