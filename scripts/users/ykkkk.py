a97500cf4b2ad4600905003fb1b2957bfaa82bc0
GaiShen <1214241948@qq.com>
2025-12-09 16:30:36 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is ahead of 'origin/main' by 3 commits.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   pages/src/all_data.json
    	new file:   scripts/users/ykkkk.py

---
+++ scripts/users/ykkkk.py
@@ -0,0 +1,21 @@
+import sys
+import os
+# Add the parent directory to the path so we can import from main
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from main import *
+if __name__ == "__main__":
+    ps=paths.PathS()
+    user=ps.users_path
+
+
+    # 抓取用户ac题目数
+
+    atcoderData.atcoder_gets(user,ps.atcoder_path_user)
+    # codeforcesData.codeforces_gets(user,ps.codeforces_path_user)
+    # matijiData.matiji_gets(user,ps.matiji_path_user)
+
+    # # 地址列表生成
+    # file_list_generator.generate_file_list_json(ps.root_path)
+    # # 合并
+    data_converter.convert_data(ps.root_path,user,ps.out_path)
